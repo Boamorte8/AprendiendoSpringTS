28c4690e8330379cc5ee14c2c6b3075177b58536
Esteban Salazar <esteban.salazar@pragma.com.co>
2018-01-26 11:44:55 -0500

    Se cambia el proyecto para que me genere un .war y poderlo desplegar en otros servidores

--- src/main/java/com/boamorte/profesoresplatzi/controller/MainController.java
+++ src/main/java/com/boamorte/profesoresplatzi/controller/MainController.java
@@ -1,10 +1,10 @@
 package com.boamorte.profesoresplatzi.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
 
 	@RequestMapping("/")
